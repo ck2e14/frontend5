d66cb66f8142946bcd100d63d63079263de34853
ck2e14 <chriswkennedy@icloud.com>
2020-07-22 05:05:30 +0100

    fixing API routes

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,6 +1,6 @@
 import App from '../App'
 
-const API_URL = "http://localhost:3000/api/v1/";
+const API_URL = "https://mod5-project-backend-api.herokuapp.com//api/v1/";
 const LOGIN_URL = `${API_URL}login`;
 const VALIDATE_URL = `${API_URL}validate`;
 const SIGNUP_URL = `${API_URL}users`;
